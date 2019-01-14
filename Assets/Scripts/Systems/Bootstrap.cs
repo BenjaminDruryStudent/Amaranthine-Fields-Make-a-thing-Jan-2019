ff45c7eb9011f33da05425114756037b3d2ef925
Benjamin Drury <benjamin.m.drury@gmail.com>
2019-01-14 23:28:32 +1000

    Finished Shader. Generating Grid

--- Assets/Scripts/Systems/Bootstrap.cs
+++ Assets/Scripts/Systems/Bootstrap.cs
@@ -8,28 +8,43 @@ namespace AmaranthineFields
 {
     public class Bootstrap : MonoBehaviour
     {
-        public float speed;
+        public int size;
+        public float3 rotation;
         public Mesh mesh;
         public Material material;
-        Entity[] entities;
 
         private void Start()
         {
             EntityManager entityManager = World.Active.GetOrCreateManager<EntityManager>();
 
-            for (int i = 0; i < 10; i++)
-            {
-                Entity playerEntity = entityManager.CreateEntity(
-                                ComponentType.Create<Position>(),
-                                ComponentType.Create<MeshInstanceRenderer>()
-                                );
+            EntityArchetype entityArchetype = entityManager.CreateArchetype(
+                typeof(Position),
+                typeof(Rotation),
+                typeof(Scale),
+                typeof(Static),
+                typeof(MeshInstanceRenderer)
+                );
 
-                entityManager.SetSharedComponentData(playerEntity, new MeshInstanceRenderer
+            material.SetFloat("_icon_texture_index", 0);
+
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
                 {
-                    mesh = mesh,
-                    material = material
-                });
+                    Entity entity = entityManager.CreateEntity(entityArchetype);
+
+                    Quaternion rot = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
+
+                    entityManager.SetComponentData(entity, new Position { Value = new float3((-size/2)+x,(size/2)-y,0) });
+                    entityManager.SetComponentData(entity, new Rotation { Value = new float4(rot.x, rot.y, rot.z, rot.w) });
+                    entityManager.SetComponentData(entity, new Scale { Value = new float3(0.1f,1,0.1f)});
+                    entityManager.SetSharedComponentData(entity, new MeshInstanceRenderer
+                    {
+                        mesh = mesh,
+                        material = material
+                    });
+                }
             }
         }
-    } 
+    }
 }
