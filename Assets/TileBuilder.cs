43e80f7908975a962b6e2049b5c3793bf9ebc560
Benjamin Drury <benjamin.m.drury@gmail.com>
2019-01-15 14:29:07 +1000

    Working on Visualisation

---
+++ Assets/TileBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBuilder : MonoBehaviour {
+
+    public int spritePixelsX, spritePixelsY;
+    
+    public int xGridSize,yGridSize;
+    public GameObject prefab;
+    GameObject[] cards;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    [ContextMenu("BuildCards")]
+    public void BuildCards()
+    {
+        cards = new GameObject[xGridSize * yGridSize];
+
+        GameObject newCard = Instantiate<GameObject>(prefab, Vector3.zero, Quaternion.identity, transform);
+
+        cards[0] = newCard;
+    }
+}
