6732b08f5bff92c01d9add253b2f363ced7cfcd2
Benjamin Drury <benjamin.m.drury@gmail.com>
2019-01-15 13:48:24 +1000

    Looking at diferent ways to display

---
+++ Assets/GridRenderer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridRenderer : MonoBehaviour {
+    public string displayText;
+
+    private void OnValidate()
+    {
+        GetComponent<Text>().text = displayText;
+    }
+}
